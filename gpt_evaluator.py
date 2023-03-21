5704ea18704b47085ce18ed73083380bddeb0ff1
cbrwx <81207429+cbrwx@users.noreply.github.com>
2023-03-21 07:17:02 +0100

    Update gpt_evaluator.py

--- gpt_evaluator.py
+++ gpt_evaluator.py
@@ -140,9 +140,11 @@ def generate_report(model_engine, bleu_score, rouge_scores, wer_score, file_path
     report += "- Custom Metric (if applicable): [brief explanation]\n\n"
 
     report += "4. Results\n"
-    report += f"BLEU Score: {bleu_score} (higher is better)\n"
-    report += f"ROUGE Scores: {rouge_scores} (higher is better)\n"
-    report += f"Word Error Rate (WER): {wer_score} (lower is better)\n\n"
+    report += f"- BLEU Score: {bleu_score:.2f} ({bleu_score*100:.2f}%)\n"
+    report += f"- ROUGE-1 Score: {rouge_scores['rouge-1']:.2f} ({rouge_scores['rouge-1']*100:.2f}%)\n"
+    report += f"- ROUGE-2 Score: {rouge_scores['rouge-2']:.2f} ({rouge_scores['rouge-2']*100:.2f}%)\n"
+    report += f"- ROUGE-L Score: {rouge_scores['rouge-l']:.2f} ({rouge_scores['rouge-l']*100:.2f}%)\n"
+    report += f"- Word Error Rate (WER): {wer_score:.2f} ({wer_score*100:.2f}%)\n\n"
 
     report += "5. Conclusion\n"
     report += "Based on the evaluation results, the GPT model shows [summary of performance]. [Any additional insights, recommendations, or conclusions].\n"
